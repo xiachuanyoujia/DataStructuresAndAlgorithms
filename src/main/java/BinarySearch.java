b8adda0a0b3af65eb7d8396889e0842f172c8c97
xiachuanyoujia <1243357202@qq.com>
2023-11-23 16:25:08 +0800

    fix:除2改位运算>>>1

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -16,7 +16,7 @@ public class BinarySearch {
     public static int binarySearchBasic(int[] a, int target) {
         int i = 0, j = a.length - 1;
         while (i <= j) {
-            int m = (i + j) / 2;
+            int m = (i + j) >>> 1;
             if (target < a[m]) {
                 j = m - 1;
             } else if (a[m] < target) {
