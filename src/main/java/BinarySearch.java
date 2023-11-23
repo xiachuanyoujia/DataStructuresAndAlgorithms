ea8feba72d0f953c42ba5eabea41d0b89a5e5fd8
xiachuanyoujia <1243357202@qq.com>
2023-11-23 15:46:53 +0800

    init

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class BinarySearch {
+
+    public static int binarySearchBasic(int[] a, int target) {
+
+        return -1;
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
