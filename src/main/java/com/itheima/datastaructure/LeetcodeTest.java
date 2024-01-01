2449a6f5a70bb1a97c0871f3ee16cd0bc722e2a3
xiachuanyoujia <1243357202@qq.com>
2024-01-01 14:08:42 +0800

    feat：leetCode HOT100  20.有效的括号

---
+++ src/main/java/com/itheima/datastaructure/LeetcodeTest.java
@@ -0,0 +1,43 @@
+package com.itheima.datastaructure;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+public class LeetcodeTest {
+
+    /**
+     * leetCode HOT100  20.有效的括号
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                Character top = stack.pop();
+                if (c == ')' && top != '(') return false;
+                if (c == ']' && top != '[') return false;
+                if (c == '}' && top != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    public void testIsValid() {
+        String s = "()[]{}";
+        isValid(s);
+    }
+
+}
