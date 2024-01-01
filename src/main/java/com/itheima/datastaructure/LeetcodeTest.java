2d4667cc74fec9288eddfa060133c4b784b4bfcf
xiachuanyoujia <1243357202@qq.com>
2024-01-01 14:34:11 +0800

    feat：leetCode HOT100  21. 合并两个有序链表

--- src/main/java/com/itheima/datastaructure/LeetcodeTest.java
+++ src/main/java/com/itheima/datastaructure/LeetcodeTest.java
@@ -6,6 +6,68 @@ import java.util.Stack;
 
 public class LeetcodeTest {
 
+
+    /**
+     * leetCode HOT100  21. 合并两个有序链表
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return dummy.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    @Test
+    public void testMergeTwoLists() {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+        mergeTwoLists(list1, list2);
+    }
+
+
     /**
      * leetCode HOT100  20.有效的括号
      * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
