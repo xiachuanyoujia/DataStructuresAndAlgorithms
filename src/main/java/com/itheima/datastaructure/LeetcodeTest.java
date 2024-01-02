53ed33af2220de9e51a44c60e5340d089dd25338
xiachuanyoujia <1243357202@qq.com>
2024-01-02 11:33:33 +0800

    leetCode HOT100  141.环形链表

--- src/main/java/com/itheima/datastaructure/LeetcodeTest.java
+++ src/main/java/com/itheima/datastaructure/LeetcodeTest.java
@@ -2,9 +2,39 @@ package com.itheima.datastaructure;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Stack;
 
 public class LeetcodeTest {
+
+    /**
+     * leetCode HOT100  141.环形链表
+     * 给你一个链表的头节点 head ，判断链表中是否有环。
+     * <p>
+     * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+     * <p>
+     * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next==null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 
 
     /**
