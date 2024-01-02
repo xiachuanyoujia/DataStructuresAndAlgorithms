3162493a0d9fa38e093bf59e6964dcd8665e2a92
xiachuanyoujia <1243357202@qq.com>
2024-01-02 15:15:14 +0800

    feat：leetCode HOT100  206. 反转链表

--- src/main/java/com/itheima/datastaructure/LeetcodeTest.java
+++ src/main/java/com/itheima/datastaructure/LeetcodeTest.java
@@ -11,14 +11,20 @@ public class LeetcodeTest {
 
 
     /**
-     * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
-     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     * leetCode HOT100  206. 反转链表
+     * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
      *
-     * @param nums
+     * @param head
      * @return
      */
-    public int majorityElement(int[] nums) {
-
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newListNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newListNode;
     }
 
     /**
