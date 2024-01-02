1184682fa53b2c4ac0cedd89dd2837439f4dbd3e
xiachuanyoujia <1243357202@qq.com>
2024-01-02 10:39:20 +0800

    feat：递归插入排序1

---
+++ src/main/java/com/itheima/algorithm/recursion/E05InsertionSort.java
@@ -0,0 +1,44 @@
+package com.itheima.algorithm.recursion;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+public class E05InsertionSort {
+
+    public static void sort(int[] a) {
+        insertion(a, 1);
+    }
+
+    //j代表未排序区域右边界
+    private static void insertion(int[] a, int low) {
+        if (low == a.length) {
+            return;
+        }
+
+        int t = a[low];
+        int i = low - 1;
+
+        while (i >= 0 && a[i] > t) {
+            a[i + 1] = a[i];
+            i--;
+        }
+
+        a[i + 1] = t;
+
+        insertion(a, low + 1);
+    }
+
+    @Test
+    public void test1() {
+        int[] unsortedArray = {5, 3, 1, 4, 2};
+        int[] expectedArray = {1, 2, 3, 4, 5};
+
+        E05InsertionSort.sort(unsortedArray);
+
+        assertArrayEquals(expectedArray, unsortedArray);
+    }
+}
