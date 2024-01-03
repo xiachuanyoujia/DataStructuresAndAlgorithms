6f924af9bb3b2e7c6f38a1719d8ebce7201bdfda
xiachuanyoujia <1243357202@qq.com>
2024-01-03 14:43:56 +0800

    feat：leetCode HOT100  70.爬楼梯

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -1,13 +1,32 @@
-package com.itheima.datastaructure;
+package com.itheima;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LeetcodeTest {
+
+
+    /**
+     * leetCode HOT100  70.爬楼梯
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n - 1; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
 
     /**
      * leetCode  HOT100  136. 只出现一次的数字
