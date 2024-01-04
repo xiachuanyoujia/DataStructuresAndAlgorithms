adab605147be466af956e93e74d0df667213a586
xiachuanyoujia <1243357202@qq.com>
2024-01-04 13:47:00 +0800

    feat：leetCode HOT100  169.多数元素

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -11,6 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOT100  169.多数元素
+     * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+     * <p>
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int votes = 0;
+        int x = 0;
+        for (int num : nums) {
+            if (votes == 0) {
+                x = num;
+            }
+            votes = num == x ? votes + 1 : votes - 1;
+        }
+        return x;
+    }
+
+    /**
      * leetCode HOT100  94.二叉树的中序遍历
      * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
      *
