af9f80a507e2e57ae484a675b93ab544d751c918
xiachuanyoujia <1243357202@qq.com>
2024-01-04 14:37:09 +0800

    feat：leetCode HOT100  226.翻转二叉树

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -11,6 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOT100  226.翻转二叉树
+     * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /**
      * leetCode HOT100  169.多数元素
      * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
      * <p>
