9ff0669b46b5a51b08c37bd9266cc6f2f41600d8
xiachuanyoujia <1243357202@qq.com>
2024-01-04 19:29:59 +0800

    feat：leetCode HOT100  121.买卖股票的最佳时机

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -11,6 +11,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOT100  121.买卖股票的最佳时机
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * <p>
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * <p>
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int profit = 0;
+        int cost = prices[0];
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < cost) {
+                cost = prices[i];
+            }
+            if (prices[i] - cost > profit) {
+                profit = prices[i] - cost;
+            }
+        }
+        return profit;
+    }
+
+    /**
      * leetCode HOT100  104.二叉树的最大深度
      * 给你一个二叉树的根节点 root ，请你返回其最大深度。
      *
