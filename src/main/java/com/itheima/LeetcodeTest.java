cab527c5617ef948828006959f47f7440a8c725a
xiachuanyoujia <1243357202@qq.com>
2024-01-05 15:36:37 +0800

    feat：leetCode HOT100  283. 移动零

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -10,6 +10,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOT100  283. 移动零
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * <p>
+     * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+     *
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null) {
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
+            }
+        }
+    }
+
+    /**
      * leetCode HOT100 234. 回文链表
      * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
      *
