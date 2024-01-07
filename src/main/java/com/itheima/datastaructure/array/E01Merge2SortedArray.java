75fa0306d61424f01059f100c5d70110d1917862
xiachuanyoujia <1243357202@qq.com>
2024-01-07 16:36:13 +0800

    feat：合并有序数组

---
+++ src/main/java/com/itheima/datastaructure/array/E01Merge2SortedArray.java
@@ -0,0 +1,61 @@
+package com.itheima.datastaructure.array;
+
+import java.util.Arrays;
+
+public class E01Merge2SortedArray {
+
+    public static void main(String[] args) {
+        int[] a1 = {1, 5, 6, 2, 4, 10, 11};
+        int[] a2 = new int[a1.length];
+
+//        merge(a1, 0, 2, 3, 6, a2, 0);
+        merge(a1, 0, 2, 3, 6, a2);
+        System.out.println(Arrays.toString(a2));
+    }
+
+    //方法二
+    public static void merge(int[] a1, int i, int iEnd, int j, int jEnd, int[] a2) {
+        int k = 0;
+        while (i <= iEnd && j <= jEnd) {
+            if (a1[i] < a1[j]) {
+                a2[k] = a1[i];
+                i++;
+            } else {
+                a2[k] = a1[j];
+                j++;
+            }
+            k++;
+        }
+        if (i > iEnd) {
+            System.arraycopy(a1, j, a2, k, jEnd - j + 1);
+        }
+        if (j > jEnd) {
+            System.arraycopy(a1, i, a2, k, iEnd - i + 1);
+        }
+    }
+
+
+    /*
+    a1原始数组 a2结果数组   (k)
+    i,iEnd第一个有序区间的起点终点, j,jEnd第二个有序区间的起点终点
+     */
+    //方法一
+    public static void merge(int[] a1, int i, int iEnd, int j, int jEnd,
+                             int[] a2, int k) {
+        if (i > iEnd) {
+            System.arraycopy(a1, j, a2, k, jEnd - j + 1);
+            return;
+        }
+        if (j > jEnd) {
+            System.arraycopy(a1, j, a2, k, iEnd - i + 1);
+            return;
+        }
+        if (a1[i] < a1[j]) {
+            a2[k] = a1[i];
+            merge(a1, i + 1, iEnd, j, jEnd, a2, k + 1);
+        } else {
+            a2[k] = a1[j];
+            merge(a1, i, iEnd, j + 1, jEnd, a2, k + 1);
+        }
+    }
+}
