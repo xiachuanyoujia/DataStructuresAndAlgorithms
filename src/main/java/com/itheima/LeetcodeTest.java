98d6ad103a0eecc6459021430c44780fa6b30b22
xiachuanyoujia <1243357202@qq.com>
2024-03-06 15:27:39 +0800

    feat：leetcood HOP100 11. 盛最多水的容器

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LeetcodeTest {
 
+    /**
+     * leetcood HOP100 11. 盛最多水的容器
+     * <p>
+     * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+     * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * 返回容器可以储存的最大水量。
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int min = Math.min(height[left], height[right]);
+            max = Math.max(max, (right - left) * min);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    @Test
+    public void maxAreaTest() {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int maxArea = maxArea(height);
+        System.out.println(maxArea);
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
         binaryToDecimal();
