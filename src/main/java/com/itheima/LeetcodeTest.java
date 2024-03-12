cd3b2bde9898b6d10692f0a97c19c69a0cad88ae
xiachuanyoujia <1243357202@qq.com>
2024-03-12 09:37:16 +0800

    feat: leetCode HOP100 48. 旋转图像

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100 48. 旋转图像
+     * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+     * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+     *
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            return;
+        }
+        int nums = matrix.length;
+        for (int i = 0; i < nums; i++) {
+            for (int j = 0; j < nums - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - j][nums - 1 - i];
+                matrix[nums - 1 - j][nums - 1 - i] = temp;
+            }
+        }
+        for(int i=0;i<(nums>>1);i++){
+            for(int j=0;j<nums;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - i][j];
+                matrix[nums - 1 - i][j] = temp;
+            }
+        }
+    }
+
+    /**
      * leetCode HOP100  46. 全排列
      * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
      *
