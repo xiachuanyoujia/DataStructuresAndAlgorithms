23d9b25e83a25bd90a9b53cf8a32100eeb488954
xiachuanyoujia <1243357202@qq.com>
2024-03-15 11:44:18 +0800

    feat: leetCode HOP100  62. 不同路径

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100  62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[] f = new int[n];
+        for (int i = 0; i < n; i++) {
+            f[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[j] += f[j - 1];
+            }
+        }
+        return f[n - 1];
+    }
+
+    /**
      * leetCode HOP100  56. 合并区间
      * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]。
      * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
