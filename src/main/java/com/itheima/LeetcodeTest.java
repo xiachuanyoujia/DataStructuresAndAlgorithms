c1358a917a490dd9ec576e0439c0a22ab5ead3fa
xiachuanyoujia <1243357202@qq.com>
2024-03-23 10:14:31 +0800

    feat: leetCode HOP100  238. 除自身以外数组的乘积

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100  238. 除自身以外数组的乘积
+     * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+     * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+     * 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] l = new int[len];
+        int[] r = new int[len];
+        int[] answer = new int[len];
+        l[0] = 1;
+        for (int i = 1; i < len; i++) {
+            l[i] = nums[i - 1] * l[i - 1];
+        }
+        r[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            r[i] = nums[i + 1] * r[i + 1];
+        }
+        for (int i = 0; i < len; i++) {
+            answer[i] = l[i] * r[i];
+        }
+        return answer;
+    }
+
+    /**
      * leetCode HOP100  236. 二叉树的最近公共祖先
      * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
      * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
