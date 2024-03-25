acfeed5fc4c020fca53de64cafc9dcea68e4505b
xiachuanyoujia <1243357202@qq.com>
2024-03-25 13:25:06 +0800

    feat: leetCode HOP100  322. 零钱兑换

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100  322. 零钱兑换
+     * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+     * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+     * 你可以认为每种硬币的数量是无限的。
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount]>amount ? -1 : dp[amount];
+    }
+
+    /**
      * leetCode HOP100  240. 搜索二维矩阵 II
      * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
      * 每行的元素从左到右升序排列。
@@ -21,8 +48,8 @@ public class LeetcodeTest {
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = matrix.length - 1, j = 0;
         while (i >= 0 && j < matrix[0].length) {
-            if(matrix[i][j]>target) i--;
-            else if(matrix[i][j]<target) j++;
+            if (matrix[i][j] > target) i--;
+            else if (matrix[i][j] < target) j++;
             else return true;
         }
         return false;
