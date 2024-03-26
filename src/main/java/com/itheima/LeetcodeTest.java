953adb41c650d645703f127a3ede0813640ba614
xiachuanyoujia <1243357202@qq.com>
2024-03-26 09:20:18 +0800

    feat: leetCode HOP100  279. 完全平方数

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100  279. 完全平方数
+     * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+     * 全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+     *
+     * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, f[i - j * j]);
+            }
+            f[i] = min + 1;
+        }
+        return f[n];
+    }
+
+    /**
      * leetCode HOP100  322. 零钱兑换
      * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
      * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
@@ -32,7 +55,7 @@ public class LeetcodeTest {
                 }
             }
         }
-        return dp[amount]>amount ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
     /**
