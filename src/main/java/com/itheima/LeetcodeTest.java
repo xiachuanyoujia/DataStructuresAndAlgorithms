e7a814f0ecdeb7e1e697a3d53a723d0d67dc2cbd
xiachuanyoujia <1243357202@qq.com>
2024-03-28 09:51:38 +0800

    feat: leetCode HOP100  287. 寻找重复数

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * leetCode HOP100  287. 寻找重复数
+     * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+     * 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+     * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    /**
      * leetCode HOP100  279. 完全平方数
      * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
      * 全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
