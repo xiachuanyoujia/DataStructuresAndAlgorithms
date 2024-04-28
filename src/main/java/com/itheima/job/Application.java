ef0f47665dde6dc8952dfe9058e2a4a8a05fe6db
xiachuanyoujia <1243357202@qq.com>
2024-04-28 12:14:17 +0800

    feat: [java作业]求圆柱的表面积和三棱柱的表面积进阶

--- src/main/java/com/itheima/job/Application.java
+++ src/main/java/com/itheima/job/Application.java
@@ -2,6 +2,7 @@ package com.itheima.job;
 
 abstract class Geometry {
     abstract double getArea();
+    abstract double getLength();
 }
 
 class Circle extends Geometry {
@@ -15,21 +16,29 @@ class Circle extends Geometry {
     double getArea() {
         return Math.PI * r * r;
     }
+
+    @Override
+    double getLength() {
+        return 2 * Math.PI * r;
+    }
 }
 
 class Triangle extends Geometry {
-    double a, b, c;
+    double bottom, height;
 
-    Triangle(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    Triangle(double bottom, double height) {
+        this.bottom = bottom;
+        this.height = height;
     }
 
     @Override
     double getArea() {
-        double p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return 0.5 * bottom * height;
+    }
+
+    @Override
+    double getLength() {
+        return bottom + 2 * Math.sqrt(Math.pow(height, 2) + Math.pow(bottom / 2, 2));
     }
 }
 
@@ -43,7 +52,7 @@ class Pillar {
     }
 
     double getSurfaceArea() {
-        return bottom.getArea() * 2 + bottom.getArea() * height;
+        return bottom.getArea() * 2 + bottom.getLength() * height;
     }
 }
 
@@ -52,7 +61,7 @@ public class Application {
         Pillar pillar1 = new Pillar(new Circle(2), 10);
         System.out.println("以半径2为底的圆柱的表面积为：" + pillar1.getSurfaceArea());
 
-        Pillar pillar2 = new Pillar(new Triangle(1, 1, 1), 10);
-        System.out.println("三角形三边长度为1的三棱柱的表面积为：" + pillar2.getSurfaceArea());
+        Pillar pillar2 = new Pillar(new Triangle(2, 1), 10);
+        System.out.println("三角形底为2高为1的三棱柱的表面积为：" + pillar2.getSurfaceArea());
     }
 }
