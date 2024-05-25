60d563ecd4cf1686173c19a22b137fd0a32a6fd0
xiachuanyoujia <1243357202@qq.com>
2024-05-25 22:08:43 +0800

    feat: 面试题 01.06. 字符串压缩

--- src/main/java/com/itheima/interviewQuestion/easy.java
+++ src/main/java/com/itheima/interviewQuestion/easy.java
@@ -7,6 +7,41 @@ import java.util.*;
 public class easy {
 
     /**
+     * 面试题 01.06. 字符串压缩
+     * 字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
+     *
+     * @param S
+     * @return
+     */
+    public String compressString(String S) {
+        if (S.length() == 0) { // 空串处理
+            return S;
+        }
+        StringBuffer ans = new StringBuffer();
+        int cnt = 1;
+        char ch = S.charAt(0);
+        for (int i = 1; i < S.length(); ++i) {
+            if (ch == S.charAt(i)) {
+                cnt++;
+            } else {
+                ans.append(ch);
+                ans.append(cnt);
+                ch = S.charAt(i);
+                cnt = 1;
+            }
+        }
+        ans.append(ch);
+        ans.append(cnt);
+        return ans.length() >= S.length() ? S : ans.toString();
+    }
+
+    @Test
+    public void compressStringTest() {
+        easy easy = new easy();
+        System.out.println(easy.compressString("aabcccccaa"));
+    }
+
+    /**
      * 面试题 01.04. 回文排列
      * 给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
      * 回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列。
