68750d90e7f56e6e5aa7e87ad62f433bb4aa85c2
xiachuanyoujia <1243357202@qq.com>
2024-06-16 10:15:50 +0800

    feat: 63. 不同路径 II

--- src/main/java/com/itheima/dp/dpTest.java
+++ src/main/java/com/itheima/dp/dpTest.java
@@ -2,9 +2,54 @@ package com.itheima.dp;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class dpTest {
+
+    /**
+     * 63. 不同路径 II
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[i][0] == 1 || dp[i - 1][0] == 0) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[0][i] == 1 || dp[0][i - 1] == 0) {
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+
+    @Test
+    public void testUniquePathsWithObstacles() {
+        int[][] obstacleGrid = {
+                {0, 0, 1, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}
+        };
+        uniquePathsWithObstacles(obstacleGrid);
+    }
 
     /**
      * 62. 不同路径
