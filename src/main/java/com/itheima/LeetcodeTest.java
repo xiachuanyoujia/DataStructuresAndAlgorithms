f273779a8d4a8b079425484c34dcf33d8d133768
xiachuanyoujia <1243357202@qq.com>
2024-06-27 13:19:32 +0800

    feat: 349. 两个数组的交集

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * 349. 两个数组的交集
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1.length == 0 || nums2.length == 0) return new int[0];
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> res = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (!set.contains(nums1[i])) {
+                set.add(nums1[i]);
+            }
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                res.add(nums2[i]);
+            }
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    @Test
+    public void intersectionTest() {
+        intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+    }
+
+    /**
      * 242. 有效的字母异位词
      *
      * @param s
