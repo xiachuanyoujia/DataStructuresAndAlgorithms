220a3702383d367f0d9d4417fda43890b0d4f110
xiachuanyoujia <1243357202@qq.com>
2024-07-21 13:16:20 +0800

    feat: 15. 三数之和

--- src/main/java/com/itheima/LeetcodeTest.java
+++ src/main/java/com/itheima/LeetcodeTest.java
@@ -9,6 +9,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeetcodeTest {
 
     /**
+     * 15. 三数之和
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int k = 0; k < nums.length - 2; k++) {
+            if (nums[k] > 0) break;
+            if (k > 0 && nums[k] == nums[k - 1]) continue;
+            int l = k + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[k] + nums[l] + nums[r];
+
+                if (sum < 0) {
+                    while (l < r && nums[l] == nums[++l]) ;
+                } else if (sum > 0) {
+                    while (l < r && nums[r] == nums[--r]) ;
+                } else {
+                    res.add(new ArrayList<>(Arrays.asList(nums[k], nums[l], nums[r])));
+                    while (l < r && nums[l] == nums[++l]) ;
+                    while (l < r && nums[r] == nums[--r]) ;
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
      * 454. 四数相加 II
      *
      * @param nums1
